7eeaccd5fd57e84175bd3b4a43dfb0776722846f
Roland ALIMA <ralima@ansetech.com>
2018-09-09 12:58:30 +0200

    Premières améliorations de la vue

--- app/src/main/java/cm/mboasoftwares/roland/resistanceequivalente/MainActivity.java
+++ app/src/main/java/cm/mboasoftwares/roland/resistanceequivalente/MainActivity.java
@@ -4,7 +4,9 @@ import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,6 +14,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import static android.widget.RelativeLayout.BELOW;
+import static android.widget.RelativeLayout.END_OF;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -33,13 +38,22 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     String val1, val2, val3, val4, val5, val6;
 
+    TextView resultText;
     TextView result;
+    TextView resultUnit;
     TextView toleranceText;
     TextView toleranceTv;
     TextView coefTempText;
     TextView coefTempTv;
     TextView coefTempUnit;
     Button evaluate;
+
+    TextView band1Text;
+    TextView band2Text;
+    TextView band3Text;
+    TextView band4Text;
+    TextView band5Text;
+    TextView band6Text;
 
     ArrayAdapter<CharSequence> colors_anneau2_adapter;
     ArrayAdapter<CharSequence> colors_anneau3_adapter;
@@ -56,7 +70,74 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         int width = size.x;
         int height = size.y;
 
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(width/3, LinearLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams blp1 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp1.addRule(BELOW, R.id.colors_choice_text);
+        blp1.addRule(Gravity.CENTER);
+
+        RelativeLayout.LayoutParams blp2 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp2.addRule(BELOW, R.id.colors_choice_text);
+        blp2.addRule(Gravity.CENTER);
+        blp2.addRule(END_OF, R.id.band1_text);
+
+        RelativeLayout.LayoutParams blp3 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp3.addRule(BELOW, R.id.colors_choice_text);
+        blp3.addRule(Gravity.CENTER);
+        blp3.addRule(END_OF, R.id.band2_text);
+
+        RelativeLayout.LayoutParams blp4 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp4.addRule(BELOW, R.id.color1_spinner);
+        blp4.addRule(Gravity.CENTER);
+
+        RelativeLayout.LayoutParams blp5 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp5.addRule(BELOW, R.id.color1_spinner);
+        blp5.addRule(Gravity.CENTER);
+        blp5.addRule(END_OF, R.id.band4_text);
+
+        RelativeLayout.LayoutParams blp6 = new RelativeLayout.LayoutParams(width/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+        blp6.addRule(BELOW, R.id.color1_spinner);
+        blp6.addRule(Gravity.CENTER);
+        blp6.addRule(END_OF, R.id.band5_text);
+
+        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp1.addRule(BELOW, R.id.band1_text);
+
+        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp2.addRule(BELOW, R.id.band1_text);
+        lp2.addRule(END_OF, R.id.color1_spinner);
+
+        RelativeLayout.LayoutParams lp3 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp3.addRule(BELOW, R.id.band1_text);
+        lp3.addRule(END_OF, R.id.color2_spinner);
+
+        RelativeLayout.LayoutParams lp4 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp4.addRule(BELOW, R.id.band4_text);
+        //lp4.addRule(END_OF, R.id.color2_spinner);
+
+        RelativeLayout.LayoutParams lp5 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp5.addRule(BELOW, R.id.band4_text);
+        lp5.addRule(END_OF, R.id.color4_spinner);
+
+        RelativeLayout.LayoutParams lp6 = new RelativeLayout.LayoutParams(width/3, 150);
+        lp6.addRule(BELOW, R.id.band4_text);
+        lp6.addRule(END_OF, R.id.color5_spinner);
+
+        band1Text = findViewById(R.id.band1_text);
+        band1Text.setLayoutParams(blp1);
+
+        band2Text = findViewById(R.id.band2_text);
+        band2Text.setLayoutParams(blp2);
+
+        band3Text = findViewById(R.id.band3_text);
+        band3Text.setLayoutParams(blp3);
+
+        band4Text = findViewById(R.id.band4_text);
+        band4Text.setLayoutParams(blp4);
+
+        band5Text = findViewById(R.id.band5_text);
+        band5Text.setLayoutParams(blp5);
+
+        band6Text = findViewById(R.id.band6_text);
+        band6Text.setLayoutParams(blp6);
 
         toleranceTv = (TextView) findViewById(R.id.tolerance_value);
         toleranceText = (TextView) findViewById(R.id.tolerance_text);
@@ -65,6 +146,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         coefTempTv = (TextView) findViewById(R.id.coefTempe_value);
         coefTempUnit = (TextView) findViewById(R.id.coefTempe_unit);
 
+        resultText = findViewById(R.id.result_text);
+        resultUnit = findViewById(R.id.result_unit);
         result = (TextView) findViewById(R.id.result);
         evaluate = (Button) findViewById(R.id.evaluate_button);
         evaluate.setOnClickListener(new View.OnClickListener() {
@@ -99,33 +182,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         color1_spinner.setAdapter(colors_anneau1_adapter);
         color1_spinner.setVisibility(View.INVISIBLE);
         //color1_spinner.setOnItemSelectedListener(this);
-        //color1_spinner.setLayoutParams(lp);
+        color1_spinner.setLayoutParams(lp1);
 
         color2_spinner = (Spinner) findViewById(R.id.color2_spinner);
         color2_spinner.setAdapter(colors_anneau2_adapter);
         color2_spinner.setVisibility(View.INVISIBLE);
         //color2_spinner.setOnItemSelectedListener(this);
-        //color2_spinner.setLayoutParams(lp);
+        color2_spinner.setLayoutParams(lp2);
 
         color3_spinner = (Spinner) findViewById(R.id.color3_spinner);
         color3_spinner.setAdapter(colors_anneau3_adapter);
         color3_spinner.setVisibility(View.INVISIBLE);
         //color3_spinner.setOnItemSelectedListener(this);
+        color3_spinner.setLayoutParams(lp3);
 
         color4_spinner = (Spinner) findViewById(R.id.color4_spinner);
         color4_spinner.setAdapter(colors_anneau4_adapter);
         color4_spinner.setVisibility(View.INVISIBLE);
         //color4_spinner.setOnItemSelectedListener(this);
+        color4_spinner.setLayoutParams(lp4);
 
         color5_spinner = (Spinner) findViewById(R.id.color5_spinner);
         color5_spinner.setAdapter(colors_anneau5_adapter);
         color5_spinner.setVisibility(View.INVISIBLE);
         //color5_spinner.setOnItemSelectedListener(this);
+        color5_spinner.setLayoutParams(lp5);
 
         color6_spinner = (Spinner) findViewById(R.id.color6_spinner);
         color6_spinner.setAdapter(colors_anneau6_adapter);
         color6_spinner.setVisibility(View.INVISIBLE);
         //color1_spinner.setOnItemSelectedListener(this);
+        color6_spinner.setLayoutParams(lp6);
     }
 
     @Override
@@ -141,12 +228,18 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 color4_spinner.setVisibility(View.INVISIBLE);
                 color5_spinner.setVisibility(View.INVISIBLE);
                 color6_spinner.setVisibility(View.INVISIBLE);
+                resultText.setVisibility(View.INVISIBLE);
+                resultUnit.setVisibility(View.INVISIBLE);
+                result.setVisibility(View.INVISIBLE);
                 result.setText("");
                 toleranceText.setVisibility(View.INVISIBLE);
                 toleranceTv.setVisibility(View.INVISIBLE);
                 coefTempText.setVisibility(View.INVISIBLE);
                 coefTempTv.setVisibility(View.INVISIBLE);
                 coefTempUnit.setVisibility(View.INVISIBLE);
+                band4Text.setVisibility(View.INVISIBLE);
+                band5Text.setVisibility(View.INVISIBLE);
+                band6Text.setVisibility(View.INVISIBLE);
                 break;
             case 4:
                 nbrNoeux = 4;
@@ -158,9 +251,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 color4_spinner.setAdapter(colors_anneau4_adapter);
                 color5_spinner.setVisibility(View.INVISIBLE);
                 color6_spinner.setVisibility(View.INVISIBLE);
+                band4Text.setVisibility(View.VISIBLE);
+                band5Text.setVisibility(View.INVISIBLE);
+                band6Text.setVisibility(View.INVISIBLE);
+                resultText.setVisibility(View.INVISIBLE);
+                resultUnit.setVisibility(View.INVISIBLE);
+                result.setVisibility(View.INVISIBLE);
                 result.setText("");
-                toleranceText.setVisibility(View.VISIBLE);
-                toleranceTv.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.INVISIBLE);
+                toleranceTv.setVisibility(View.INVISIBLE);
                 toleranceTv.setText("");
                 coefTempText.setVisibility(View.INVISIBLE);
                 coefTempTv.setVisibility(View.INVISIBLE);
@@ -175,10 +274,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 color4_spinner.setVisibility(View.VISIBLE);
                 color4_spinner.setAdapter(colors_anneau3_adapter);
                 color5_spinner.setVisibility(View.VISIBLE);
+                band4Text.setVisibility(View.VISIBLE);
+                band5Text.setVisibility(View.VISIBLE);
+                band6Text.setVisibility(View.INVISIBLE);
                 color6_spinner.setVisibility(View.INVISIBLE);
+                resultText.setVisibility(View.INVISIBLE);
+                resultUnit.setVisibility(View.INVISIBLE);
+                result.setVisibility(View.INVISIBLE);
                 result.setText("");
-                toleranceText.setVisibility(View.VISIBLE);
-                toleranceTv.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.INVISIBLE);
+                toleranceTv.setVisibility(View.INVISIBLE);
                 toleranceTv.setText("");
                 coefTempText.setVisibility(View.INVISIBLE);
                 coefTempTv.setVisibility(View.INVISIBLE);
@@ -194,13 +299,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 color4_spinner.setAdapter(colors_anneau3_adapter);
                 color5_spinner.setVisibility(View.VISIBLE);
                 color6_spinner.setVisibility(View.VISIBLE);
+                band4Text.setVisibility(View.VISIBLE);
+                band5Text.setVisibility(View.VISIBLE);
+                band6Text.setVisibility(View.VISIBLE);
+                resultText.setVisibility(View.INVISIBLE);
+                resultUnit.setVisibility(View.INVISIBLE);
+                result.setVisibility(View.INVISIBLE);
                 result.setText("");
-                toleranceText.setVisibility(View.VISIBLE);
-                toleranceTv.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.INVISIBLE);
+                toleranceTv.setVisibility(View.INVISIBLE);
                 toleranceTv.setText("");
-                coefTempText.setVisibility(View.VISIBLE);
-                coefTempTv.setVisibility(View.VISIBLE);
-                coefTempUnit.setVisibility(View.VISIBLE);
+                coefTempText.setVisibility(View.INVISIBLE);
+                coefTempTv.setVisibility(View.INVISIBLE);
+                coefTempUnit.setVisibility(View.INVISIBLE);
                 coefTempTv.setText("");
                 break;
             default:
@@ -219,6 +330,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 val1 = anneau1[color1_spinner.getSelectedItemPosition()];
                 val2 = anneau2[color2_spinner.getSelectedItemPosition()];
                 val3 = multiplicator[color3_spinner.getSelectedItemPosition()];
+                resultText.setVisibility(View.VISIBLE);
+                resultUnit.setVisibility(View.VISIBLE);
+                result.setVisibility(View.VISIBLE);
                 double v3 = Double.parseDouble(val1 + val2)*Double.parseDouble(val3);
                 result.setText(String.valueOf(v3));
                 break;
@@ -227,6 +341,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 val2 = anneau2[color2_spinner.getSelectedItemPosition()];
                 val3 = multiplicator[color3_spinner.getSelectedItemPosition()];
                 val4 = tolerance[color4_spinner.getSelectedItemPosition()];
+                resultText.setVisibility(View.VISIBLE);
+                resultUnit.setVisibility(View.VISIBLE);
+                result.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.VISIBLE);
+                toleranceTv.setVisibility(View.VISIBLE);
                 double v4 = Double.parseDouble(val1 + val2)*Double.parseDouble(val3);
                 result.setText(String.valueOf(v4));
                 String tol4 = "\u00B1 " + val4;
@@ -238,6 +357,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 val3 = anneau2[color3_spinner.getSelectedItemPosition()];
                 val4 = multiplicator[color4_spinner.getSelectedItemPosition()];
                 val5 = tolerance[color5_spinner.getSelectedItemPosition()];
+                resultText.setVisibility(View.VISIBLE);
+                resultUnit.setVisibility(View.VISIBLE);
+                result.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.VISIBLE);
+                toleranceTv.setVisibility(View.VISIBLE);
                 double v5 = Double.parseDouble(val1 + val2 + val3)*Double.parseDouble(val4);
                 result.setText(String.valueOf(v5));
                 String tol5 = "\u00B1 " + val5;
@@ -250,6 +374,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 val4 = multiplicator[color4_spinner.getSelectedItemPosition()];
                 val5 = tolerance[color5_spinner.getSelectedItemPosition()];
                 val6 = coeffTemp[color6_spinner.getSelectedItemPosition()];
+                resultText.setVisibility(View.VISIBLE);
+                resultUnit.setVisibility(View.VISIBLE);
+                result.setVisibility(View.VISIBLE);
+                toleranceText.setVisibility(View.VISIBLE);
+                toleranceTv.setVisibility(View.VISIBLE);
+                coefTempText.setVisibility(View.VISIBLE);
+                coefTempTv.setVisibility(View.VISIBLE);
+                coefTempUnit.setVisibility(View.VISIBLE);
                 double v6 = Double.parseDouble(val1 + val2 + val3)*Double.parseDouble(val4);
                 result.setText(String.valueOf(v6));
                 String tol6 = "\u00B1 " + val5;
